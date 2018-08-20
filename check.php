b0763909eb011e658628766c287c1dfcbbc628fe
Olga Soboleva <olgasoboleva99@gmail.com>
2018-08-21 01:10:17 +0300

    sending by ajax

--- check.php
+++ check.php
@@ -1,9 +1,10 @@
-<?php
+<?php                                
 	session_start();            
 	$x = $_GET["X"];
 	$y = $_GET["Y"];
 	$r = $_GET["R"];
-	$check;                   
+	$check;   
+	$symbol;                
 	$currentTime = date('H:i:s',strtotime('-1 hour'));
 	$runtime = microtime(true); 
 	if (!isset($_SESSION['tableArray'])) {
@@ -14,19 +15,29 @@
 <head>
 	<meta charset="utf-8">
 	<style type="text/css">
+		body {                         
+			font-family: monospace;
+		}
 		table {
 			border-radius:4px; 
 			border: double 4px #555;
-			font-size: 18px; 
+			font-size: 18px;       
 			text-align: center;
 			margin-top: 30px;	
 		}
 		.parametr {
 			width: 10%;
 		}
+		.classTrue{
+			color: #00ff00;
+		}
+		.classFalse{
+			color: #ff0000;
+		}
 	</style>
 </head>
 <body>
+	<H2 align="center">Результаты</H2>
 	<table border="1" cellpadding="0" cellspacing="0" width="80%" align="center" margin="auto">
 		<tr>
 			<th class="parametr">X</th><th class="parametr">Y</th><th class="parametr">R</th>
@@ -41,11 +52,13 @@
 			($x <= 0 && $y >= 0 && (pow($x,2)+pow($y,2)) <= pow($r,2)) || 
 			($x <= 0 && $x >= -$r && $y <= 0 && $y >= -$r/2) || 
 			($x >= 0 && $y >= 0 && $y <= ($r/2-$x))) {
-			$check = "True";
+			$check = "classTrue";
+			$symbol = "&#10004;";
 		}
 				
 		else  {
-			$check = "False";
+			$check = "classFalse";
+			$symbol = "&#215;";
 		}
 	}
 	else {
@@ -57,7 +70,7 @@
 		unset($_SESSION['tableArray'][0]);
 		$_SESSION['tableArray'] = array_values($_SESSION['tableArray']);		
 	}
-	$_SESSION['tableArray'][] = "<tr><td>$x</td><td>$y</td><td>$r</td><td>$check</td><td>$currentTime</td><td>$runtime</td></tr>";
+	$_SESSION['tableArray'][] = "<tr><td>$x</td><td>$y</td><td>$r</td><td class=\"$check\">$symbol</td><td>$currentTime</td><td>$runtime</td></tr>";
 	foreach($_SESSION['tableArray'] as $row){
 		echo $row;
 	}
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<?php                                         
 	session_start();
 ?>
 <!DOCTYPE html>
@@ -27,6 +27,7 @@
 			font-size: 18px;
 			padding-top: 10px;  
 			border-radius: 30px;
+			border-color: none;
 			background: #ddd; 
 			border-collapse: collapse;   
 		}
@@ -61,7 +62,8 @@
 			background: #ddd;
 			border:solid 1px #999;
 			box-shadow:0 1px 5px rgba(0,0,0,0.2);                                   
-			background: -webkit-gradient(linear, 0 0, 0 100%, from(#ddd), to(#ccc));                                       
+			background: -webkit-gradient(linear, 0 0, 0 100%, from(#ddd), to(#ccc));
+			margin-bottom: 20px;                                       
 		} 
 		input[type="button"]:active {      
 			box-shadow:none;
@@ -80,8 +82,8 @@
 			padding-bottom: 5%;
 		}           
 		span {
-			display: none;
-			font-size: 12px;
+			display: inline;
+			font-size: 11px;
 			color: #ff0000;
 		}               
 		#footer {                                                                       
@@ -93,24 +95,32 @@
 			height: 100%; 
 			width: 80%;
 		}
+		a:link,a:visited,a:active { 
+			text-decoration: none; 
+			color: #444;	
+		}                        
+		a:hover {
+			text-decoration: underline;
+			color: #444;
+		}
 	</style>
 </head>
 
-<body onunload="<?php session_destroy()?>">
+<body onunload="<?php session_destroy()?>" onload="createPanel('canvas',0,0,0);scaling();ajaxSend();">
 	<table id="content" border="0" cellpadding="0" cellspacing="0">
 		<tr>    
 		        <th colspan="2">
 				<table width="100%">
 					<tr>
-						<td>Группа P3212</td>
-						<td>Соболева О.А.</td>      
+						<td><a href="https://isu.ifmo.ru/pls/apex/f?p=2143:GR:116320706676860::NO::GR_GR,GR_DATE:p3112,20.08.2018" target="_blank">Группа P3212</a></td>
+						<td><a href="https://isu.ifmo.ru/pls/apex/f?p=2143:PERSON:116320706676860::NO:RP:PID:243887" target="_blank">Соболева О.А.</a></td>      
 						<td>Вариант 28211</td>
 					</tr>
 				</table>
 			</th>
 		</tr>
 		<tr height="350" valign="bottom">
-			<td><img src="graph.png" height="300"></td>                              
+			<td><canvas id="canvas" style="background-color:#ffffff; border-radius: 20px;" width="300" height="300"></canvas></td>                              
 			<td>    
 				<form class="from" action="check.php" method="get" id="formXYR" target="resultFrame" onkeydown="if(event.keyCode==13){return false;}">
 				  <fieldset>
@@ -131,8 +141,10 @@
 					<label for="Y"> Y : </label>
  		        		<input class="formFields" id="Y" type="text" name="Y" placeholder="(-3 .. 3)" maxlength="8" 
 						required title="Введите значение в промежутке от -3 до 3" onfocus="clearWarnings();"><br>
+
+					<span id="warning"><br/></span>
                                                                                          
-					<table class="formFields">
+					<table>
         					<tr>
                   					<td rowspan="2"> R : </td>
 	        					<td>
@@ -148,9 +160,8 @@
         						<td></td><td>
 							<input onchange="clearWarnings();likeRadiobox(this);" class="checkbox" type="checkbox" id="ch5" name="R" value=5>5</td>
 	        				</tr>
-              				</table> 
-					<span id="radiusSpan">Выберите ровно один радиус!</span><br>                                                                                                     
-					<input class="formFields" type="button" name="button" value="Проверить" onclick="validation()">
+              				</table>                                                                                                                                                         
+					<input class="formFields" type="button" name="button" id="button" value="Проверить" >
 				  </fieldset>
         			</form>
 			</td>
@@ -169,39 +180,30 @@
 	</table>                                                                                                                               
 		
 	<script type="text/javascript">
-		function validation(){
+		function validation(X,Y,R,rChecked){
 			var form = document.forms[0];
-			var chBoxes = document.getElementsByClassName("checkbox");
-			var rChecked = 0;
 			var success = true;
-			var R = 0;
-			for (var i=0;i<chBoxes.length;i++){
-				if (chBoxes[i].checked){
-					rChecked++;
-					R = chBoxes[i].value;
-				}
-			}
-			var X = form.X.value;
-			var Y = form.Y.value;
 			if (rChecked!=1){                                                  
-				success = false;
-				document.getElementById('radiusSpan').style.display="inline";
+				success = false;                                          
 			}
-			if (Y>=3 || Y<=-3 || Y.length>8 || isNaN(Y) || Y.length<1){          
-			        document.getElementById('Y').style.border="1px solid red";                                             
+			if (Y>=3 || Y<=-3 || Y.length>8 || isNaN(Y) || Y.length<1 || Y.charAt(0)=="."){          
+			        document.getElementById('Y').style.border="1px solid red";                                                                    
 				success = false;
+				if (Y.length<1) document.getElementById('warning').innerHTML = "Введите X!";
+				else if (Y.search(/^[0-9]+\,[0-9]+$/) != -1) document.getElementById('warning').innerHTML = "Дробная часть отделяется точкой!"; 
+				else if (Y>=3) document.getElementById('warning').innerHTML = "Значение меньше 3!";
+				else if (Y<=3) document.getElementById('warning').innerHTML = "Значение больше -3!";
+				else if (isNaN(Y)||Y.charAt(0)==".") document.getElementById('warning').innerHTML = "X - целое или дробное число!";
 			} else {
 				document.getElementById('Y').style.border="1px solid green";
 			}
 			
-			if (success){                                                        
-				document.getElementById("formXYR").submit();                         	                	
-			}
+			return success;
 		}  
 
 		function clearWarnings(){                                   
 			document.getElementById('Y').style.border="1px solid #ccc";
-			document.getElementById('radiusSpan').style.display="none";
+			document.getElementById('warning').innerHTML = "<br/>";
 		} 
          
 		function resizeFrame(){                                                                     
@@ -219,6 +221,169 @@
 				}
 			}
 		}
+
+
+		function ajaxSend(){
+			var btn = document.getElementById('button');
+  			var frame = document.getElementById('resultFrame');
+                                                
+			var request = new XMLHttpRequest();	
+  			request.onreadystatechange = function() {
+				if(request.readyState == 4) {
+					if(request.status == 200) {
+						frame.srcdoc = request.responseText; 
+						frame.style.display="block";
+						       
+					} else {
+						frame.innerHTML = 'Произошла ошибка при запросе: ' +  request.status + ' ' + request.statusText;
+					}
+				}
+			}                   
+
+			btn.addEventListener('click', function() {
+				var url = 'check.php?';
+				var R = 0;
+				var form = document.forms[0];
+				var chBoxes = document.getElementsByClassName("checkbox");
+				var rChecked = 0;
+			
+				for (var i=0;i<chBoxes.length;i++){
+					if (chBoxes[i].checked){
+						rChecked++;
+						R = chBoxes[i].value;
+					}
+				}
+				var X = form.X.value;
+				var Y = form.Y.value;
+				url += 'X='+X+'&Y='+Y+'&R='+R;
+				
+				// определяем тип запроса
+				request.open('Get', url);
+			
+				if (validation(X,Y,R,rChecked)){
+					// отправляем запрос
+					createPanel('canvas',X,Y,R);
+					request.send();
+				}
+			});	
+		}
+
+		function scaling(){
+			if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+				document.getElementById("content").style.width = "80%";
+			}
+		}
+
+		function createPanel(id,x,y,r){
+			var canvas = document.getElementById(id),      
+			ctx = canvas.getContext("2d");
+			size = canvas.width;                 
+			ctx.clearRect(0, 0, size, size);
+
+			if (r!=0){
+				//сектор
+				ctx.beginPath();
+				ctx.moveTo(size/2,size/2);
+				ctx.arc(size/2,size/2,size*2/5,Math.PI,Math.PI*3/2,false);
+				ctx.closePath();
+				ctx.fillStyle = "blue";
+				ctx.strokeStyle = "blue";
+				ctx.fill();
+				ctx.stroke();
+	
+				//треугольник
+				ctx.beginPath();
+				ctx.moveTo(size/2,size/2-size/5);
+				ctx.lineTo(size/2+size/5,size/2);
+				ctx.lineTo(size/2,size/2);
+				ctx.lineTo(size/2,size/2-size/5);
+				ctx.closePath();                           
+				ctx.fill();
+				ctx.stroke();
+	
+				//прямоугольник
+				ctx.beginPath();           
+				ctx.fillRect(size/10,size/2,size*2/5,size/5);
+				ctx.closePath(); 
+
+				ctx.fillStyle = "black";
+				ctx.fillText("Y",size/2+size/20, size/30);
+				ctx.fillText("X",size-size/30,size/2+size/10);	
+				ctx.fillText(-r,size/10-size/30, size/2-size/15);
+				ctx.fillText(-r/2,size*3/10-size/30, size/2-size/15);
+				ctx.fillText(r/2,size*7/10-size/30, size/2-size/15);
+				ctx.fillText(r,size*9/10-size/30, size/2-size/15);
+				ctx.fillText(-r,size/2+size/30, size*9/10+size/30);
+				ctx.fillText(-r/2,size/2+size/30, size*7/10+size/30);
+				ctx.fillText(r/2,size/2+size/20, size*3/10+size/30);
+				ctx.fillText(r,size/2+size/20, size/10+size/30); 
+				
+			}           
+			else {
+	       			ctx.font = "16px Calibri";  //подписи к осям   			ctx.fillStyle = "black";
+				ctx.fillText("Y",size/2+size/20, size/30);
+				ctx.fillText("X",size-size/30,size/2+size/10);
+				ctx.fillText("-R",size/10-size/30, size/2-size/15);
+				ctx.fillText("-R/2",size*3/10-size/30, size/2-size/15);
+				ctx.fillText("R/2",size*7/10-size/30, size/2-size/15);
+				ctx.fillText("R",size*9/10-size/30, size/2-size/15);
+				ctx.fillText("-R",size/2+size/30, size*9/10+size/30);
+				ctx.fillText("-R/2",size/2+size/30, size*7/10+size/30);
+				ctx.fillText("R/2",size/2+size/20, size*3/10+size/30);
+				ctx.fillText("R",size/2+size/20, size/10+size/30);                       	
+			}
+
+			//оси
+			ctx.beginPath();
+			ctx.moveTo(size/2,0);
+			ctx.lineTo(size/2,size);
+			ctx.moveTo(0,size/2);
+			ctx.lineTo(size,size/2);
+
+			ctx.moveTo(size/2,0);      //стрелки
+			ctx.lineTo(size/2-size/30,size/30);
+			ctx.moveTo(size/2,0);
+			ctx.lineTo(size/2+size/30,size/30);
+                        ctx.moveTo(size,size/2);      
+			ctx.lineTo(size-size/30,size/2-size/30);
+			ctx.moveTo(size,size/2);
+			ctx.lineTo(size-size/30,size/2+size/30);
+
+
+			ctx.moveTo(size/2+size/5,size/2-size/60);    //деления оси X
+			ctx.lineTo(size/2+size/5,size/2+size/60);
+			ctx.moveTo(size/2+size*2/5,size/2-size/60);
+			ctx.lineTo(size/2+size*2/5,size/2+size/60);                
+			ctx.moveTo(size/2-size/5,size/2-size/60);
+			ctx.lineTo(size/2-size/5,size/2+size/60);
+			ctx.moveTo(size/2-size*2/5,size/2-size/60);
+			ctx.lineTo(size/2-size*2/5,size/2+size/60);
+
+			ctx.moveTo(size/2-size/60,size/2+size/5); //деления оси Y
+			ctx.lineTo(size/2+size/60,size/2+size/5);
+			ctx.moveTo(size/2-size/60,size/2+size*2/5);
+			ctx.lineTo(size/2+size/60,size/2+size*2/5);
+			ctx.moveTo(size/2-size/60,size/2-size/5);
+			ctx.lineTo(size/2+size/60,size/2-size/5);
+			ctx.moveTo(size/2-size/60,size/2-size*2/5);
+			ctx.lineTo(size/2+size/60,size/2-size*2/5);
+			
+			ctx.closePath();
+			ctx.fillStyle = "black";
+			ctx.strokeStyle = "black";
+			ctx.fill();
+			ctx.stroke();
+
+			if (r!=0){
+				//точка
+				ctx.beginPath();
+				ctx.arc(size/2+x*size*2/(5*r),size/2-y*size*2/(5*r),2,0,2*Math.PI)
+				ctx.fillStyle = "red";
+				ctx.fill();
+				ctx.closePath();
+			}                         
+		}
+
 		                                                      
 	</script>
 </body>
